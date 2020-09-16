dd12347db95b65b2161c5e5e5a0f810f9ed04adb
zhicheng <zhichengzhouenfrance@gmail.com>
2020-09-16 14:33:11 +0200

    second commit

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,18 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'blog',
+        loadChildren: './blog/blog.module#BlogBlogModule'
+      },
+      {
+        path: 'post',
+        loadChildren: './post/post.module#BlogPostModule'
+      },
+      {
+        path: 'tag',
+        loadChildren: './tag/tag.module#BlogTagModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
